c87f65adbde9353ec985ec9562b3f73786863d48
Benjamin <benjamin.d.scharf@gmail.com>
2017-08-26 20:51:42 -0500

    Added very simple prototype for updating post set for reddit.

--- lib/reddit.py
+++ lib/reddit.py
@@ -1,7 +1,12 @@
 import links
+import praw
 
 class SubReddit(links.NamedLink):
     posts = set()
+    included = set()
 
     def __init__(self, url, name):
         links.NamedLink.__init__(self, url, name)
+
+    def updatePosts(self):
+        return
