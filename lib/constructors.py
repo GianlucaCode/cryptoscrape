ff6573d91936c3b904b58987189a314a19990b7a
Benjamin <benjamin.d.scharf@gmail.com>
2017-08-26 20:16:18 -0500

    Add some constructors for board operations.

---
+++ lib/constructors.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import os, datetime
+
+class Link():
+    url = ""
+
+    def __init__(self, url):
+        self.url = url
+
+class NamedLink(Link):
+    name = ""
+
+    def __init__(self, url, name):
+        Link.__init__(self, url)
+        self.name = name
+
+class Thread(NamedLink):
+    timestamp
+    replyCount = 0 
+    replies = set()
+
+    def __init__(self, url, name, timestamp, replyCount):
+        NamedLink.__init__(self, url, name)
+        self.timestamp = timestamp
+        self.replyCount = replyCount
+        
+
