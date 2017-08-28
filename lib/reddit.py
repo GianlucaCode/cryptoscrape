d082be98c99fb0571ec8088b3227fe00a0d69d5d
Benjamin <benjamin.d.scharf@gmail.com>
2017-08-27 21:41:05 -0500

    Create new Reddit class.

--- lib/reddit.py
+++ lib/reddit.py
@@ -1,5 +1,16 @@
 import links
+import source
 import praw
+
+INCLUDED_PATH_REDDIT = "../sources/reddit/include.txt"
+
+class Reddit(source.Source):
+    subreddits = set()
+
+    def __init__(self):
+        source.Source.__init__(self, INCLUDED_PATH_REDDIT)
+        for sr in self.included:
+            subreddits.add("/r/" + sr)
 
 class SubReddit(links.NamedLink):
     posts = set()
