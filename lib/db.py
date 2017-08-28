1f5a68170f649d0f187cde6faccfe0f2c9dc4cd5
Benjamin <benjamin.d.scharf@gmail.com>
2017-08-28 17:18:06 -0500

    Add file-reading functionality for command execution.

--- lib/db.py
+++ lib/db.py
@@ -7,13 +7,17 @@ def create_connection(db_path):
         return connection
     except Error as e:
         print(e)
-   
+
     return None
 
-def execute_sql(connection, sql_file):
+def execute_sql(connection, sql_filepath):
+    # read in sql from file at path
+    with open(sql_filepath, "r") as sql_file:
+        command = sql_file.read().replace('\n', '')
+
     try:
         c = connection.cursor()
-        c.execute(sql_file)
+        c.execute(command)
     except Error as e:
         print(e)
 
@@ -21,4 +25,3 @@ def setup():
     database = "cryptos.db"
 
     connection = create_connection(database)
-
