e9edf25b0e0aa82dc1ef2e24e03be50911666c66
Benjamin <benjamin.d.scharf@gmail.com>
2017-08-28 17:07:19 -0500

    Add basic database connection and execution functionality.

--- lib/db.py
+++ lib/db.py
@@ -1,2 +1,21 @@
 import sqlite3
 from sqlite3 import Error
+
+def create_connection(db_path):
+    try:
+        connection = sqlite3.connect(db_path)
+        print("Connected to database with version "  + sqlite3.version)
+
+    except Error as e:
+        print(e)
+
+    finally:
+        connection.close()
+
+def execute_sql(connection, sql_file):
+    try:
+        c = connection.cursor()
+        c.execute(sql_file)
+    except Error as e:
+        print(e)
+
