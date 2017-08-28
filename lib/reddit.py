8011d0d18ffefff188d3fd2e1f4a0c7987a01249
Benjamin <benjamin.d.scharf@gmail.com>
2017-08-28 18:35:32 -0500

    Give each Reddit object it's own access to praw.

--- lib/reddit.py
+++ lib/reddit.py
@@ -2,14 +2,14 @@ import links
 import source
 import json, requests, praw
 
-aReddit = praw.Reddit('crypto-scrape')
-
 INCLUDED_PATH_REDDIT = "../sources/reddit/include.txt"
 
 class Reddit(source.Source):
     subreddits = set()
+    instance = praw.Reddit("crypto-scrape")
 
     def __init__(self):
         source.Source.__init__(self, INCLUDED_PATH_REDDIT)
         for sr in self.included:
-            subreddits.add("/r/" + sr)
+            subreddits.add(self.instance.subreddit(sr))
+
