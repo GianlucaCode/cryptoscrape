c45c9402c9703de9beca6fd357264a7851337775
Benjamin <benjamin.d.scharf@gmail.com>
2017-08-27 21:47:37 -0500

    Remove subreddit class for now.

--- lib/reddit.py
+++ lib/reddit.py
@@ -11,12 +11,3 @@ class Reddit(source.Source):
         source.Source.__init__(self, INCLUDED_PATH_REDDIT)
         for sr in self.included:
             subreddits.add("/r/" + sr)
-
-class SubReddit(links.NamedLink):
-    posts = set()
-
-    def __init__(self, url, name):
-        links.NamedLink.__init__(self, url, name)
-
-    def updatePosts(self):
-        return
