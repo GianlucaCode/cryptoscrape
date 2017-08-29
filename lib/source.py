f4829edc5f8b4253df565b3a0e12fc1e0c9bbebc
Benjamin <benjamin.d.scharf@gmail.com>
2017-08-28 19:19:46 -0500

    Fix updateRun to read and write to file correctly.

--- lib/source.py
+++ lib/source.py
@@ -4,7 +4,7 @@ class Source():
     included = set([])
     includePath = ""
     lastRunPath = ""
-    lastRun = ""
+    lastRun = 0
 
     def __init__(self, includePath, lastRunPath, included=[]):
         self.included = set(included)
@@ -22,9 +22,10 @@ class Source():
         if os.path.isfile(self.lastRunPath):
             with open(self.lastRunPath) as f:
                 for line in f:
-                    self.lastRun += line.split("\n")[0]
+                    self.lastRun = int(line.split("\n")[0])
         # if file does not exist, it is created with a timestamp of 0
         else:
             with open(self.lastRunPath,"w+") as f:
                 f.write("0")
+                self.lastRun = 0
                 f.close()
