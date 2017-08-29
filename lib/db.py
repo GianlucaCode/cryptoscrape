79c436833801764222e904a2032c7e1a36f21661
Benjamin <benjamin.d.scharf@gmail.com>
2017-08-28 23:25:18 -0500

    Fix SQL execution; now works with parameters (held in list)

--- lib/db.py
+++ lib/db.py
@@ -10,14 +10,19 @@ def create_connection(db_path):
 
     return None
 
-def execute_sql(connection, sql_filepath):
+def execute_sql(connection, sql_filepath, params=[]):
     # read in sql from file at path
     with open(sql_filepath, "r") as sql_file:
         command = sql_file.read().replace('\n', '')
 
     try:
         c = connection.cursor()
+        c.execute("SELECT * FROM cryptos;")
+        if len(params) > 0:
+            command = command % tuple(params)
+
         c.execute(command)
+        connection.commit()
     except Error as e:
         print(e)
 
@@ -25,3 +30,4 @@ def setup():
     database = "cryptos.db"
 
     connection = create_connection(database)
+    execute_sql(connection, "lib/sql/create_crypto_table.sql")
