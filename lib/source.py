065e6b3c5a9db84baf089796fabf865b9b981211
Benjamin <benjamin.d.scharf@gmail.com>
2017-08-28 19:13:30 -0500

    Add new lastRun variable to be updated and used when scraping.

--- lib/source.py
+++ lib/source.py
@@ -1,15 +1,30 @@
+import os
+
 class Source():
     included = set([])
     includePath = ""
-    mentions = dict()
-    
-    def __init__(self, includePath, included=[]):
+    lastRunPath = ""
+    lastRun = ""
+
+    def __init__(self, includePath, lastRunPath, included=[]):
         self.included = set(included)
         self.includePath = includePath
-        self.updateIncluded(self.includePath)
+        self.lastRunPath = lastRunPath
+        self.updateIncluded()
+        self.updateRun()
 
-    def updateIncluded(self, path):
-        with open(path) as f:
+    def updateIncluded(self):
+        with open(self.includePath) as f:
             for line in f:
                 self.included.add(line.split("\n")[0])
 
+    def updateRun(self):
+        if os.path.isfile(self.lastRunPath):
+            with open(self.lastRunPath) as f:
+                for line in f:
+                    self.lastRun += line.split("\n")[0]
+        # if file does not exist, it is created with a timestamp of 0
+        else:
+            with open(self.lastRunPath,"w+") as f:
+                f.write("0")
+                f.close()
