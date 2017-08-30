5f838dd3be7d9726915435e3182f7770268cf721
Benjamin <benjamin.d.scharf@gmail.com>
2017-08-29 20:33:20 -0500

    Track currency mentioned in post or comment as well.

--- lib/sql/create_reddit_comments_table.sql
+++ lib/sql/create_reddit_comments_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS reddit_comments(
 	sub text NOT NULL,
+	currency text NOT NULL,
 	post_id text NOT NULL,
 	contents text NOT NULL,
 	sentiment decimal,
--- lib/sql/create_reddit_posts_table.sql
+++ lib/sql/create_reddit_posts_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS reddit_posts(
 	sub text NOT NULL,
+	currency text NOT NULL,
 	contents text NOT NULL,
 	sentiment decimal,
 	subjectivity decimal
