ad59ec8dc49c26b939b8ba0ab54aab7610f1b267
Benjamin <benjamin.d.scharf@gmail.com>
2017-09-30 18:43:14 -0500

    Now logs post information and uses command-line arguments.

--- run.py
+++ run.py
@@ -1,10 +1,22 @@
 from lib import *
+import argparse
+
+parser = argparse.ArgumentParser(description="Select a post limit for reddit collection.")
+parser.add_argument('integer', metavar='limit', type=int, nargs='+',help='the post limit')
+args = parser.parse_args()
+
+postLimit = 0
+
+if (len(args.integer) > 0):
+	postLimit = args.integer[0]
+
+print("Searching %i posts..." % (postLimit))
 
 db.setup()
 
 anInstance = reddit.Reddit()
 
-postLimit = 20 
-
 anInstance.collectMentions(postLimit)
 anInstance.writeMentions()
+
+print("Done.")
